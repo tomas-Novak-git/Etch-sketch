44730f6b49499c08538bbd8a606fb6353c3c698d
Yatrikoxi <tomas_novak@centrum.cz>
2023-04-18 09:44:29 +0200

    redid the css. no button - slider value

--- javascript.js
+++ javascript.js
@@ -1,23 +1,2 @@
-const paintBlocks = document.getElementById("paintBlock");
-const mainBlock = document.getElementById("mainBlock");
-const sixteenBtn = document.getElementById("sixteenBtn");
-const thirtyTwoBtn = document.getElementById("thirtyTwoBtn");
 const paintArea = document.getElementById("paintArea");
 
-sixteenBtn.addEventListener("click", ()=> {
-    for(let i = 0; i <= 16; i++) {
-const createDiv = document.createElement("div");
-createDiv.classList.add("paintSquare");
-paintArea.appendChild(createDiv);}
-document.querySelector("#sixteenBtn").disabled = true;
-document.querySelector("#thirtyTwoBtn").disabled = false;
-})
-
-thirtyTwoBtn.addEventListener("click", ()=> {
-    for(let i = 0; i <= 32; i++) {
-    const createDiv = document.createElement("div");
-    createDiv.classList.add("paintSquare");
-    paintArea.appendChild(createDiv);}
-    document.querySelector("#thirtyTwoBtn").disabled = true;
-    document.querySelector("#sixteenBtn").disabled = false;
-    })
