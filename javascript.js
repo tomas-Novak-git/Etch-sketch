913af6817afd0b8c88f636e935067e638b4c954d
Yatrikoxi <tomas_novak@centrum.cz>
2023-04-22 18:12:50 +0200

    added paint style buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 const valueText = document.getElementById("valueText");
 const paintSquare = document.getElementById("paintSquare");
+const paintArea = document.getElementById("paintArea");
+
 let newColor = "#ff0000";
+let dotBtn = 0;
+let lineBtn = 1;
+
 // grid squares generating func
 function createPaintArea(size){
     let paintArea = document.getElementById("paintArea");
@@ -10,13 +15,17 @@ function createPaintArea(size){
     for (let i = 0; i < numSquares; i++){
         let div = document.createElement("div");
         div.setAttribute("id", "paintSquare");
+        if (dotBtn == 0 && lineBtn == 1) {
         div.addEventListener('mouseover', function(event){
             event.target.style.backgroundColor = `${newColor}`;
         })
+        } else if (dotBtn == 1 && lineBtn == 0) {
+        div.addEventListener('mousedown', function(event){
+            event.target.style.backgroundColor = `${newColor}`;
+    })}
         paintArea.insertAdjacentElement("beforeend", div)
     }
 }
-
 // boardClean function to clear container before adding more divs.
 // Doesn't allow stacking countless divs.
 function boardClean(){
@@ -42,10 +51,33 @@ chooseColor.addEventListener('input', () => {
 // reset Button
 const resetBtn = document.getElementById("reset");
 resetBtn.addEventListener("click", resetButton);
+
 function resetButton() {  
-    const paintSquare = document.getElementById("paintSquare");
-    paintSquare.style.backgroundColor = "lightgrey";
+    let Area = paintArea.children;
+    for (let i = 0; i < size * size; i++) {
+        Area[i].style.backgroundColor = "lightgrey";
+    }
 }
+// paint type changers - DOT and LINE
+let dotButton = document.getElementById("dot");
+dotButton.addEventListener('click', dotFunc);
+
+function dotFunc() {
+    dotBtn = 1;
+    lineBtn = 0;
+    boardClean();
+    createPaintArea(size);
+}
+let lineButton = document.getElementById("line");
+lineButton.addEventListener('click', lineFunc);
+
+function lineFunc () {
+    lineBtn = 1;
+    dotBtn = 0;
+    boardClean();
+    createPaintArea(size);
+}
+
 // random color generator
 // const rainbow = document.querySelector("#randomColor");
 // rainbow.addEventListener('click', () => {
